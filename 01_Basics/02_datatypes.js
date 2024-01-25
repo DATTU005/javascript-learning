450c2a8a46b7993351c077bc3ce537eb8fb2775a
Vibhav <veebhavjoshi1@gmail.com>
2024-01-26 01:37:40 +0530

    Lesson 2 added

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,21 @@
+let string = "Vibhav";
+let age = 23;
+let isLoggedIn = false;
+
+// ​‌‍‌⁡⁢⁣⁢primitive datatypes⁡​
+/* 
+    number
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalone value
+    undefined
+    symbol
+*/
+
+// ⁡⁢⁣⁢System defined datatypes⁡
+// object
+
+console.log(typeof Object); // function
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
