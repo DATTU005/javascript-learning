2799b6eceb122f75d58ff1e7dd33ecbf4f3d371f
Vibhav <veebhavjoshi1@gmail.com>
2024-01-27 12:55:34 +0530

    conversions added

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+// ⁡⁢⁣⁢--------------------Conversion----------------------⁡
+let score = "Vibhav33";
+// console.log(score)
+
+let valueNumber = Number(score);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+let isLoggedIn = "";
+let booleanValue = Boolean(isLoggedIn);
+// console.log(typeof booleanValue);
+// console.log(booleanValue);
+
+let number = "22";
+let stringNumber = String(number);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+// ⁡⁢⁣⁢--------------------Operations----------------------⁡
