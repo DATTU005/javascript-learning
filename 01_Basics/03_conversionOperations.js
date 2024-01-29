8b96558b50c5d02a76db02b5a7929cd12c38d879
Vibhav <veebhavjoshi1@gmail.com>
2024-01-29 17:39:21 +0530

    compariosn added

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -13,7 +13,15 @@ let booleanValue = Boolean(isLoggedIn);
 
 let number = "22";
 let stringNumber = String(number);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
 
 // ⁡⁢⁣⁢--------------------Operations----------------------⁡
+let str1 = 5;
+let str2 = 6;
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3" + 4);
+console.log(1 + "2" + 3);
---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log("2" > 1);
+// console.log(2 > "2");
+
+console.log(null > 1); //false
+console.log(null == 1); // false // equality does not convert it so ans will be false
+console.log(null === 1); // false // equality does not convert it so ans will be false
+console.log(null <= 1); // true // Comparison converts null to number which is 0
+
+// console.log(undefined > 1); // false
+// console.log(undefined == 1); // false
+// console.log(undefined <= 1); // false
+
+// ===
+// console.log("2" === 2); //false because their data type is not equal
